07039413257fba03c380fd279a0a734e1e509a0f
masha antropova <masha.antropova@gmail.com>
2019-05-05 16:31:04 -0400

    Remove semicolons

--- config/terraform/files/index.js
+++ config/terraform/files/index.js
@@ -3,23 +3,23 @@ const axios = require('axios')
 
 exports.main = (req, res) => {
   function sendMessage(options) {
-    const token = localconf.telegramToken;
-    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+    const token = localconf.telegramToken
+    const url = `https://api.telegram.org/bot${token}/sendMessage`
 
     axios.post(url, {
       chat_id: options.chatId,
       text: options.greetings
     })
-    .then(function(response) {
-      res.send({ status: 'OK' });
+    .then(response => {
+      res.send({ status: 'OK' })
     })
-    .catch(function(error) {
-      res.sendStatus(500);
-    });
+    .catch(error => {
+      res.sendStatus(500)
+    })
   }
 
 
-  const callToken = req.path;
+  const callToken = req.path
 
   if (callToken !== localconf.webhookToken) {
     const message = req.body.message;
@@ -27,10 +27,10 @@ exports.main = (req, res) => {
     const options = {
       greetings: `Hello ${message.from.first_name} ${message.from.last_name}!`,
       chatId: message.chat.id
-    };
+    }
 
-    sendMessage(options);
+    sendMessage(options)
   } else {
-    res.sendStatus(403);
+    res.sendStatus(403)
   }
 }
