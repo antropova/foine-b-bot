664cb0f5039beee79cb5601162b95be6848aec5d
masha antropova <masha.antropova@gmail.com>
2019-05-05 17:00:34 -0400

    Add admin column to users table

--- database.rb
+++ database.rb
@@ -9,3 +9,10 @@ DB.create_table :users do
   String :zodiac
   String :telegram_id
 end
+
+DB.transaction do
+  DB.alter_table(:users) do
+    add_column :admin, "BOOLEAN", default: false
+  end
+  DB[:users].where(name: "Masha").first.update(admin: true)
+end
