0f015360d963ff06d8ff7e4e907dc88985e13d70
masha antropova <masha.antropova@gmail.com>
2019-06-15 16:05:13 -0400

    Send horoscope to everyone duh

--- bot.rb
+++ bot.rb
@@ -7,7 +7,7 @@ require_relative "user"
 
 DB = User.db
 users = DB[:users]
-users.where(admin: true).each do |user|
+users.each do |user|
   telegram_bot = User.new(user: user)
   telegram_bot.send_message
 end
