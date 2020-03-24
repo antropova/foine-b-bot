2b75ef5dabd1b68c30456c10a71dd12853f125ab
masha antropova <masha.antropova@gmail.com>
2020-03-24 02:17:20 -0400

    Send to those who are not paused

--- bot.rb
+++ bot.rb
@@ -8,7 +8,7 @@ require_relative 'user'
 USER_DB = User.db
 users = USER_DB[:users]
 
-users.where(admin: true).each do |user|
+users.where(paused: false).each do |user|
   telegram_bot = User.new(user: user)
   telegram_bot.send_message
 end
